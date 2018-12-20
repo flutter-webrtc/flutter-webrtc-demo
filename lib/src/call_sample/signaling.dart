466b1ca9a654f5de2e9cd0bb6440d80ea325ad8a
湖北捷智云技术有限公司 <duanweiwei1982@gmail.com>
2018-12-21 00:11:13 +0800

    Add turn server configuration example in signaling.dart.

--- lib/src/call_sample/signaling.dart
+++ lib/src/call_sample/signaling.dart
@@ -43,6 +43,14 @@ class Signaling {
   Map<String, dynamic> _iceServers = {
     'iceServers': [
       {'url': 'stun:stun.l.google.com:19302'},
+      /**
+       * turn server configuration example.
+      {
+        'url': 'turn:123.45.67.89:3478',
+        'username': 'change_to_real_user',
+        'credential': 'change_to_real_secret'
+      },
+       */
     ]
   };
 
