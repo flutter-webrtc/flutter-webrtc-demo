f4d114cd931b71b907fb42604bcc60cc882ca82e
CloudWebRTC <duanweiwei1982@gmail.com>
2019-11-21 08:44:39 +0800

    Revert "Add macOS support."

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
-import 'dart:core';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/foundation.dart'
-    show debugDefaultTargetPlatformOverride;
-
+import 'dart:core';
 import 'src/basic_sample/basic_sample.dart';
 import 'src/call_sample/call_sample.dart';
 import 'src/call_sample/data_channel_sample.dart';
 import 'src/route_item.dart';
 
-bool isDesktop() {
-  return Platform.isWindows || Platform.isLinux || Platform.isMacOS;
-}
-
-void main(){
- if(isDesktop())
-   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  runApp(new MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatefulWidget {
   @override
