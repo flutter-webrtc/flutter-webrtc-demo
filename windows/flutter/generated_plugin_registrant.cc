54771c5317fcd3a04481e0866fbb67ebe6c16256
cloudwebrtc <duanweiwei1982@gmail.com>
2021-01-29 10:06:27 +0800

    Add windows support.

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_webrtc/flutter_web_r_t_c_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterWebRTCPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterWebRTCPlugin"));
+}
