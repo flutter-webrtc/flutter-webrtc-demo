6489f474d4864cbdc8ef1680326cfd1a27af5f5d
Griffin Obeid <griffobeid@gmail.com>
2021-04-08 08:53:45 -0400

    Upgrading project to flutter 2

--- android/app/src/main/java/com/cloudwebrtc/flutterwebrtcdemo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.cloudwebrtc.flutterwebrtcdemo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/flutter_webrtc_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_webrtc_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
